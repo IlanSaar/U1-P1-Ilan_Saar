1212ada6b271a76be452d37e03cb7f630a80d31b
IlanSaar <email@email.com>
2025-09-25 14:17:43 -0400

    updated rounding and total cost

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,32 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        //Setting our variables
+
+        //Setting our variables and asking for values
+        System.out.print("Type in your subtotal: ");
         double subTotal = scan.nextDouble();
+        System.out.println();
+        System.out.print("Type in the tip percentage you would like to give: ");
         double tipPercent = scan.nextDouble();
+        System.out.println();
+        System.out.print("How many people are there: ");
         int numPeople = scan.nextInt();
+
         //Calculations
-        double tipValue = (subTotal * (tipPercent / 100);
+        double tipValue = (subTotal * (tipPercent / 100));
         double tipPerPerson = (tipValue / numPeople);
         double finalTotal = (subTotal + tipValue);
+        double totalPerPerson = (finalTotal / numPeople);
+
+        // Rounding to the nearest cent
+        tipPerPerson = Math.round((tipPerPerson + 0.005) * 100.0) / 100.0;
+        totalPerPerson = Math.round((totalPerPerson + 0.005) * 100.0) / 100.0;
+
+        //Announcing the final values
+        System.out.println("Each person will tip: $" + tipPerPerson);
+        System.out.println("Each person pays: $" + totalPerPerson);
 
     }
 }
